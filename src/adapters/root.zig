c2149ad3125c0a896b63d87f320f47bef795050c
an-npt17 <truongan1412@gmail.com>
2026-08-25 21:08:48 +0700

    update plant b

--- src/adapters/root.zig
+++ src/adapters/root.zig
@@ -3,7 +3,6 @@ pub const ads1115_probe = @import("ads1115_probe.zig");
 pub const aplay_sink = @import("aplay_sink.zig");
 pub const clip_loader = @import("clip_loader.zig");
 pub const clip_stream = @import("clip_stream.zig");
-pub const decode = @import("decode.zig");
 pub const library = @import("library.zig");
 pub const random_probe = @import("random_probe.zig");
 pub const stderr_status = @import("stderr_status.zig");
@@ -14,7 +13,6 @@ test {
     _ = aplay_sink;
     _ = clip_loader;
     _ = clip_stream;
-    _ = decode;
     _ = library;
     _ = random_probe;
     _ = stderr_status;
